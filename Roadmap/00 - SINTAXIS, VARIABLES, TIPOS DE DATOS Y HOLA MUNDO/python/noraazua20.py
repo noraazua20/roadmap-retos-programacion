d9915f2839111e222fbc44d56b3b4f23d3a3f9da
Nora Azua <100675186+noraazua20@users.noreply.github.com>
2024-07-31 18:40:10 -0500

    Create noraazua20.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/noraazua20.py
@@ -0,0 +1,37 @@
+# https://python.org
+
+# comentario de 1 línea
+
+"""
+Este es un
+comentario en varias
+lineas
+"""
+
+'''
+Este tambien es un
+comentario en varias
+lineas
+'''
+mi_variable = "mi primwer variable"
+mi_variable = "este es el nuevo contenido de mi variable"
+
+mi_constante = "mi constante" 
+
+x = 10 #int
+y = 5.678 #float
+z = 1.2e6 #notación científica
+is_true = True #booleano
+is_false = False #booleano
+mi_string = "cadena de texto" #string
+
+print("¡Hola, Python!")
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(is_true))
+print(type(is_false))
+print(type(mi_string))
+
+
